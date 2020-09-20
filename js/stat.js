1d24466288002af9cdeeaa567e737fac37932012
Kerby-vlad <53996780+Kerby-vlad@users.noreply.github.com>
2020-09-20 16:16:44 +0300

    20.09   renderStatictic with object usage

---
+++ js/stat.js
@@ -0,0 +1,85 @@
+'use strict';
+
+const NAMES = [`Катя`, `Влад`, `Вера`, `Макс`, `Оля`, `Иван`, `Коля`, `Маша`, `Вася`, `Витя`, `Вова`, `Эдик`, `Федя`, `Егор`, `Митя`, `Толя`];
+const CLOUD_WIDTH = 420;
+const CLOUD_HEIGHT = 270;
+const CLOUD_X = 100;
+const CLOUD_Y = 10;
+const GAP = 10;
+const TEXT_HEIGHT = 25;
+const TEXT_WIDTH = 80;
+const MAX_BAR_HEIGHT = CLOUD_HEIGHT - TEXT_HEIGHT * 4 - GAP;
+const barWidth = 20;
+
+function getRandomInt(max) {
+  return Math.floor(Math.random() * Math.floor(max));
+}
+
+const gamers = [
+  {
+    name: `Вы`,
+    score: getRandomInt(1000),
+    color: `rgba(255, 0, 0, 1)`
+  }
+];
+
+let MAX_SCORE = gamers[0].score;
+
+for (let i = 0; i < 4; i++) {
+  gamers.push(
+      {
+        name: NAMES[getRandomInt(7)],
+        score: getRandomInt(1000),
+        color: `hsl(246,  + ${getRandomInt(100)} + %, 50%)`
+      }
+  );
+  MAX_SCORE = gamers[i].score < MAX_SCORE ? gamers[i].score : MAX_SCORE;
+}
+
+function renderCloud(ctx, x, y, color) {
+  ctx.fillStyle = color;
+  ctx.fillRect(x, y, CLOUD_WIDTH, CLOUD_HEIGHT);
+}
+
+window.renderStatistics = function (ctx) {
+  renderCloud(
+      ctx,
+      CLOUD_X + GAP,
+      CLOUD_Y + GAP,
+      `rgba(0, 0, 0, 0.3)`
+  );
+  renderCloud(
+      ctx,
+      CLOUD_X,
+      CLOUD_Y,
+      `#fff`
+  );
+
+  ctx.fillStyle = `#000`;
+  ctx.font = `16px PT Mono`;
+  ctx.fillText(`Ура вы победили!`, CLOUD_X + GAP, CLOUD_Y + GAP + TEXT_HEIGHT);
+  ctx.fillText(`Список результатов: `, CLOUD_X + GAP, CLOUD_Y + GAP + TEXT_HEIGHT * 2);
+
+  gamers.forEach(function (gamer, index) {
+    let barHeight = MAX_BAR_HEIGHT * (MAX_SCORE / gamer.score);
+    ctx.fillStyle = `#000`;
+    ctx.fillText(
+        gamer.name,
+        CLOUD_X + GAP + (TEXT_WIDTH + GAP) * index,
+        CLOUD_Y + CLOUD_HEIGHT - GAP
+    );
+    ctx.fillStyle = gamer.color;
+    ctx.fillRect(
+        CLOUD_X + GAP + (TEXT_WIDTH + GAP) * index,
+        CLOUD_Y + CLOUD_HEIGHT - TEXT_HEIGHT - barHeight,
+        barWidth,
+        barHeight
+    );
+    ctx.fillStyle = `#000`;
+    ctx.fillText(
+        gamer.score.toString(),
+        CLOUD_X + GAP + (TEXT_WIDTH + GAP) * index,
+        CLOUD_Y + (CLOUD_HEIGHT - barHeight - GAP - TEXT_HEIGHT)
+    );
+  });
+};
